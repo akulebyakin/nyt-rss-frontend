42b883b2763bed945c372bfc4eed5932ea39399c
andrei.kulebiakin <wowand@gmail.com>
2025-04-18 20:50:22 +0200

    Add Dockerfiles and docker-compose

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  }
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const backendHost = process.env.BACKEND_HOST || env.BACKEND_HOST || 'localhost';
+    console.log('🔧 Using backend host:', backendHost);
+
+    return {
+        plugins: [
+            react(),
+            tailwindcss(),
+        ],
+        define: {
+            'process.env': {}, // предотвращает ошибки Vite с process.env
+        },
+        server: {
+            proxy: {
+                '/api': {
+                    target: `http://${backendHost}:8080`,
+                    changeOrigin: true,
+                    secure: false,
+                },
+            },
+        },
+    };
 });
